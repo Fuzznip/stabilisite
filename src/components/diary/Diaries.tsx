61392b62c9502110d4138b8eee9aebac90f34c3d
Tboodle <Tbernstein10@gmail.com>
2025-04-05 22:42:48 -0400

    cleanup

--- src/components/diary/Diaries.tsx
+++ src/components/diary/Diaries.tsx
@@ -1,10 +1,8 @@
-import { getAuthUser } from "@/lib/fetch/getAuthUser";
 import { getDiaries } from "@/lib/fetch/getDiaries";
 import { DiaryTable } from "./DiaryTable";
 import { getDiaryEntries } from "@/lib/fetch/getDiaryEntries";
 
 export default async function Diaries(): Promise<React.ReactElement> {
-  const user = await getAuthUser();
   const diaries = await getDiaries();
   const entries = await getDiaryEntries();
 
