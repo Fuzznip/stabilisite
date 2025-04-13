dd130f7cff88575c638e6c9c4686db74b7b07874
Tboodle <Tbernstein10@gmail.com>
2025-04-12 22:34:29 -0400

    should work maybe

--- src/components/diary/Diaries.tsx
+++ src/components/diary/Diaries.tsx
@@ -3,8 +3,14 @@ import { DiaryTable } from "./DiaryTable";
 import { getDiaryEntries } from "@/lib/fetch/getDiaryEntries";
 import { User } from "@/lib/types";
 
-export default async function Diaries({user}: {user?: User | null}): Promise<React.ReactElement> {
-  const diaries = await getDiaries();
+export default async function Diaries({
+  user,
+}: {
+  user?: User | null;
+}): Promise<React.ReactElement> {
+  const diaries = (await getDiaries()).filter(
+    (diary) => diary.scales.filter((scale) => scale.scale).length > 0
+  );
   const entries = await getDiaryEntries(user);
 
   return <DiaryTable diaries={diaries} entries={entries} />;
