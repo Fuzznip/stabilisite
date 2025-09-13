f4f0f092e3ddcc595e4bc60b8a0384476a4bfad4
Tboodle <Tbernstein10@gmail.com>
2025-09-12 21:53:03 -0400

    fix urls again

--- src/app/bingo/_actions/fetchBingoData.tsx
+++ src/app/bingo/_actions/fetchBingoData.tsx
@@ -6,10 +6,10 @@ export default async function fetchBingoData(): Promise<{
   board: Tile[];
   teams: Team[];
 }> {
-  const board = await fetch(`${process.env.API_URL}board`)
+  const board = await fetch(`${process.env.API_URL}/board`)
     .then((res) => res.json())
     .then(() => []);
-  const teams = await fetch(`${process.env.API_URL}teams`)
+  const teams = await fetch(`${process.env.API_URL}/teams`)
     .then((res) => res.json())
     .then((data) => (data as Team[]) || [])
     .then((teams) => teams.sort((a, b) => (b.points ?? 0) - (a.points ?? 0)))
