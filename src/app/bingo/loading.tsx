42c20111be6cbbe16e936f5a2a71a7edbac1f9e7
Tboodle <Tbernstein10@gmail.com>
2026-01-25 14:35:59 -0500

    fix skeleton

--- src/app/bingo/loading.tsx
+++ src/app/bingo/loading.tsx
@@ -25,11 +25,13 @@ function BingoBoardSkeleton() {
   );
 }
 
-function LeaderboardSkeleton() {
+function LeaderboardSkeleton({ className }: { className?: string }) {
   return (
-    <div className="flex h-full flex-col sm:min-w-[30rem] flex-shrink-0 -mt-18">
-      <span className="text-3xl text-foreground">Leaderboard</span>
-      <span className="text-xl text-muted-foreground mb-2">
+    <div
+      className={`flex h-full flex-col sm:min-w-[30rem] flex-shrink-0 ${className ?? ""}`}
+    >
+      <span className="text-2xl text-foreground">Leaderboard</span>
+      <span className="text-lg text-muted-foreground mb-2">
         Click a team to see their progress
       </span>
       <Card className="flex w-full h-full flex-col gap-4 rounded-lg relative">
@@ -49,6 +51,25 @@ function LeaderboardSkeleton() {
   );
 }
 
+function RecentDropsSkeleton() {
+  return (
+    <div className="flex w-full flex-col items-start mx-auto lg:mx-0 lg:mt-12 max-w-[80vh]">
+      <div className="flex w-full items-end justify-between mb-2">
+        <div>
+          <h2 className="text-2xl text-foreground">Recent Drops</h2>
+          <p className="text-lg text-muted-foreground">
+            All drops from the event
+          </p>
+        </div>
+        <Skeleton className="h-10 w-20 rounded-md" />
+      </div>
+      <Card className="w-full p-6 flex justify-center">
+        <Skeleton className="h-10 w-28 rounded-md" />
+      </Card>
+    </div>
+  );
+}
+
 export default function Loading(): React.ReactElement {
   return (
     <>
@@ -56,14 +77,17 @@ export default function Loading(): React.ReactElement {
         <h1 className="text-4xl font-bold">Winter Bingo 2026</h1>
         <Skeleton className="h-7 w-48" />
       </div>
-      <div className="hidden lg:flex w-full h-full flex-row items-start justify-start gap-8 z-10">
+      <div className="hidden h-0 lg:flex w-full lg:h-full flex-row items-start justify-start gap-8 z-10">
+        <BingoBoardSkeleton />
+        <div className="flex flex-col gap-8 -mt-18">
+          <LeaderboardSkeleton />
+        </div>
+      </div>
+      <div className="flex lg:hidden lg:h-0 w-full h-full flex-col justify-center items-center gap-8 z-10">
         <BingoBoardSkeleton />
         <LeaderboardSkeleton />
       </div>
-      <div className="flex lg:hidden w-full h-full flex-col justify-center items-center gap-8 pb-12 z-10">
-        <BingoBoardSkeleton />
-        <LeaderboardSkeleton />
-      </div>
+      <RecentDropsSkeleton />
     </>
   );
 }
