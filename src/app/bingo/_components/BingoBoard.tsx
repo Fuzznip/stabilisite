0e3398f5ba5d65fec3f2822179e208619590957b
Tboodle <Tbernstein10@gmail.com>
2026-01-27 15:25:17 -0500

    add tile name tooltips

--- src/app/bingo/_components/BingoBoard.tsx
+++ src/app/bingo/_components/BingoBoard.tsx
@@ -4,6 +4,11 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { Loader2 } from "lucide-react";
 import { TeamProgressResponse, TileProgress, Tile } from "@/lib/types/v2";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function BingoBoard({
   tiles,
@@ -54,44 +59,49 @@ function BingoCard({
     ? getMedalSrcForMedalLevel(progress.status.medal_level)
     : undefined;
   return (
-    <Card className="rounded-none border-2 border-bingo-grid bg-transparent shadow-none relative w-full h-full aspect-square">
-      <CardContent
-        className={cn(
-          "relative w-full h-full p-0",
-          "transition-transform duration-300 ease-in-out cursor-pointer transform hover:z-50 shadow-none hover:shadow-[0_0_20px_rgba(255,255,255,0.75)] hover:rounded-sm",
-        )}
-      >
-        <Link
-          href={`/bingo/tile/${tile?.id}`}
-          className="relative h-full w-full flex"
-        >
-          <Image
-            src={tile?.img_src || ""}
-            fill
-            priority
-            sizes="100%"
-            className="object-contain"
-            alt={`${tile?.name} tile image`}
-          />
-          {isLoading && !progress && (
-            <div className="absolute bottom-0 left-0 aspect-square size-1/2 flex items-center justify-center">
-              <Loader2 className="size-2/3 animate-spin text-stability stroke-3" />
-            </div>
-          )}
-          {medalSrc && (
-            <div className="absolute bottom-0 left-0 aspect-square h-1/2">
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Card className="rounded-none border-2 border-bingo-grid bg-transparent shadow-none relative w-full h-full aspect-square">
+          <CardContent
+            className={cn(
+              "relative w-full h-full p-0",
+              "transition-transform duration-300 ease-in-out cursor-pointer transform hover:z-50 shadow-none hover:shadow-[0_0_20px_rgba(255,255,255,0.75)] hover:rounded-sm",
+            )}
+          >
+            <Link
+              href={`/bingo/tile/${tile?.id}`}
+              className="relative h-full w-full flex"
+            >
               <Image
-                src={medalSrc}
+                src={tile?.img_src || ""}
                 fill
+                priority
                 sizes="100%"
-                className="object-contain drop-shadow-[5px_5px_5px_rgba(0,0,0,0.5)]"
-                alt="Bronze Medal"
+                className="object-contain"
+                alt={`${tile?.name} tile image`}
               />
-            </div>
-          )}
-        </Link>
-      </CardContent>
-    </Card>
+              {isLoading && !progress && (
+                <div className="absolute bottom-0 left-0 aspect-square size-1/2 flex items-center justify-center">
+                  <Loader2 className="size-2/3 animate-spin text-stability stroke-3" />
+                </div>
+              )}
+              {medalSrc && (
+                <div className="absolute bottom-0 left-0 aspect-square h-1/2">
+                  <Image
+                    src={medalSrc}
+                    fill
+                    sizes="100%"
+                    className="object-contain drop-shadow-[5px_5px_5px_rgba(0,0,0,0.5)]"
+                    alt="Bronze Medal"
+                  />
+                </div>
+              )}
+            </Link>
+          </CardContent>
+        </Card>
+      </TooltipTrigger>
+      <TooltipContent>{tile?.name}</TooltipContent>
+    </Tooltip>
   );
 }
 
